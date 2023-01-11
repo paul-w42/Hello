628fd82266e15009898f42fca79277cc6991dd40
Paul Woods <paul.n.woods@gmail.com>
2023-01-10 19:31:56 -0500

    Added index.php page

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+
+// This is my CONTROLLER for the hello project
+
+// Turn on error reporting (probably want off when live)
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+// Require the autoload file
+require_once("vendor/autoload.php");
+
+// Create an instance of the Base class
+$f3 = Base::instance();     // i.e. Base f3 = new Base() in java
+
+
+// Define a default route
+$f3->route('GET /', function() {
+    echo '<h1>Hello Fat Free!</h1>';
+});
+
+// Run Fat-Free
+$f3->run();                 // -> is the object operator, equiv to . in java
+
+
+?>
